30bf2932a20086379c2020f0f401efaa0bf1415e
muhammadkashif20 <kashif28@gmail.com>
2024-10-14 23:05:27 +0500

    I've Complete This Project on Routing & Nested Routing in Nextjs

--- src/app/main/Technology/page.js
+++ src/app/main/Technology/page.js
@@ -4,7 +4,7 @@ const page = () => {
   return <div className="flex flex-col text-center">
     <h1 className="font-semibold text-3xl mt-3  ">Our Technology </h1>  
     <p >Sharing my thoughts and ideas.</p>
-    <Link href="/main/Technology/TripleRouting">
+    <Link href="/main/Technology/TripleRoutingTechnology">
     <button className="bg-gray-900 text-white py-2 px-6 rounded-full hover:bg-gray-700 transition w-[15rem]">check Triple Routing</button>
     </Link>
 
