a8b2d4c60cda6e98fe1db1e90351041bc171cb5d
muhammadkashif20 <kashif28@gmail.com>
2024-10-14 23:11:39 +0500

    few mistakes correct

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
        <footer className="bg-gray-900 text-white py-6 mt-12">
         <div className="max-w-7xl mx-auto px-4 text-center">
-          <p>&copy; 2024 Blogging Website. All rights reserved.</p>
+          <p>copy; 2024 Blogging Website. All rights reserved.</p>
         </div>
       </footer>
     </div>
